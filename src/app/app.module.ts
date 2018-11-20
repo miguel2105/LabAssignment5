6a1ead47800a5e5d52f76258763047e9109517bc
Miguel <miguel2105@mail.fresnostate.edu>
2018-11-20 06:23:46 -0800

    Lab Assignment 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,30 @@ import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
 
+// angular material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import{MaterialDesignModule} from '../material-design/material-design.module';
+
+//routing
+import {AppRoutes} from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
 @NgModule({
   declarations: [
     AppComponent,
     MainHeaderComponent,
     MainContentComponent,
-    MainFooterComponent
+    MainFooterComponent,
+    HomeComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule,
+    AppRoutes,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-interface IStudent {
-  id?: number;
-  firstName: string;
-  lastName: string;
-  course: string;
-}
+
+
 @Component({
   selector: 'main-content',
   templateUrl: './main-content.component.html',
@@ -13,59 +9,10 @@ interface IStudent {
 })
 export class MainContentComponent implements OnInit {
 
+constructor() {
 
-  students: Array<IStudent> = [];
-  editMode: boolean = false;
+}
+  
+ngOnInit() {}
 
-
-  constructor() {
-    this.students[0] = {
-      id: 1,
-      firstName: 'Miguel',
-      lastName: 'Mendoza',
-      course: 'Programming'
-    }
-    this.students[1] = {
-      id: 2,
-      firstName: 'Tom',
-      lastName: 'Brady',
-      course: 'Swimming'
-    }
-    this.students[2] = {
-      id: 3,
-      firstName: 'Mark',
-      lastName: 'Zuc',
-      course: 'Business'
-    }
-    this.students[3] = {
-      id: 4,
-      firstName: 'Joe',
-      lastName: 'Biden',
-      course: 'Politics'
-    }
-  }
-
-  ngOnInit() {
-  }
-
-
-  addStudent() {
-    this.editMode = true;
-    const student: IStudent = {
-      id: null,
-      firstName: null,
-      lastName: null,
-      course: null
-    };
-
-    // this.students.push(student);
-    this.students.unshift(student);
-  }
-  removeStudent(index: number) {
-    console.log('from removeStudent function index: ', index);
-    this.students.splice(index, 1);
-  }
-  saveStudent() {
-    this.editMode = false;
-  }
 }
